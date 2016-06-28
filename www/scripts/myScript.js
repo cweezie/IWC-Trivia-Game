ad8157310e9d3ec7ba76c37530c6384bf439d2b6
cweezie <colomm11@gmail.com>
2016-06-28 23:53:50 +0900

    added in the rest of the calculations for javascript

--- www/scripts/myScript.js
+++ www/scripts/myScript.js
@@ -1,20 +1,41 @@
+var points = 0;
+var current; 
+
+function ScoreUp(){
+    current=10;
+    points= points + current;
+    document.getElementById("scoreCard").innerHTML = "Score:" + points;
+}
+function ScoreDown(){
+    current = 10;
+    points = points - current;
+    document.getElementById("scoreCard").innerHTML = "Score: " + points + " Points";
+}
+function Navigation(pageName){
+    window.location = pageName;
+}
 function Incorrect(){
     //grab the element we want to manipulate
     var wrongAnswer = document.getElementById("wrong1");
     wrongAnswer.style.color ="red";
+    ScoreDown();
 }
 
 function Wrong(){
     var wrongAnswer = document.getElementById("wrong2");
     wrongAnswer.style.color ="red";
+    ScoreDown();
 }
 
 function Cancel(){
     var wrongAnswer = document.getElementById("wrong3");
     wrongAnswer.style.color ="red";
+    ScoreDown();
 }
 
 function Correct(){
     var rightAnswer = document.getElementById("right1");
     rightAnswer.style.color ="green";
+    ScoreUp();
+    Navigation("ques2.html");
 }
